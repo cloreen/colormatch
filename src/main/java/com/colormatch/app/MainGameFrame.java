6745f29a3b9ab3187ad209c60842d71542ac8455
cloreen <cloreen@hotmail.com>
2016-06-25 13:39:49 -0700

    Made each color in grid unique, updated TODOS

--- src/main/java/com/colormatch/app/MainGameFrame.java
+++ src/main/java/com/colormatch/app/MainGameFrame.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -27,9 +28,9 @@ import java.util.concurrent.ThreadLocalRandom;
  * */
 
 /***
- * TODO - Ensure each button's color is unique in the grid
  * TODO - Assure target color is always among displayed colors
  * TODO - Write a reset() method to reset colors and target color
+ * DONE - Ensure each button's color is unique in the grid
  * DONE - Update target color to reflect all possible colors
  * DONE - Add logic to apply correct grammar to instruction text e.g. "Click ('a' vs 'an') xxxx button!" - changed 'a' to 'the'
  * DONE - Put project up on GitHub (public)
@@ -79,17 +80,47 @@ public class MainGameFrame extends JFrame {
 
         Dimension bDimension = new Dimension(50, 50);
         List<JButton> jButtonList = new ArrayList<JButton>();
-        JButton button1 = new JButton();
-        JButton button2 = new JButton();
-        JButton button3 = new JButton();
-        JButton button4 = new JButton();
-        JButton button5 = new JButton();
-        JButton button6 = new JButton();
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
+        JButton button4 = new JButton("4");
+        JButton button5 = new JButton("5");
+        JButton button6 = new JButton("6");
 
         final JButton[] jButtonArray = { button1, button2, button3, button4, button5, button6 };
+        List<Integer> randIntList = new ArrayList<Integer>();
+//        addToRandomIntList(jButtonArray, (ArrayList<Integer>) randIntList);
+        int[] randomInts = new Random().ints(0, 26).distinct().limit(6).toArray();
+/*
+        for (int i = 0; i < jButtonArray.length; i++) {
+            int randInt = ThreadLocalRandom.current().nextInt(0, 25 + 1);
+            if (randIntList.size() < 1) {
+                randIntList.add(randInt);
+            } else {
+                List<Integer> tempList = new ArrayList<Integer>();
+                tempList.addAll(randIntList);
+                for (int item : tempList) {
+                    if (item != randInt) {
+                        if (tempList.indexOf(item) == tempList.size()) {
+                            randIntList.add(randInt);
+                        } else {
+                            // next item in tempList
+                        }
+                    } else {
+                        int newInt = checkForDuplicate(item);
+                        if (tempList.indexOf(item) == tempList.size()) {
+                            randIntList.add(newInt);
+                        } else {
+                            // next item in tempList
+                        }
+                    }
+                }
+            }
+        }
+*/
         for (int i = 0; i < jButtonArray.length; i++) {
             jButtonArray[i].setSize(bDimension);
-            jButtonArray[i].setBackground(buttonColors[ThreadLocalRandom.current().nextInt(0, jButtonArray.length + 1)]);
+            jButtonArray[i].setBackground(buttonColors[randomInts[i]]);
             jButtonArray[i].setMnemonic(KeyEvent.VK_E);
             jButtonArray[i].setActionCommand("enable");
             final int finalI1 = i;
@@ -106,18 +137,66 @@ public class MainGameFrame extends JFrame {
             container2.add(jButtonArray[i]);
         }
 
-/*        container2.add(new Button("1"));
-        container2.add(new Button("2"));
-        container2.add(new Button("3"));
-        container2.add(new Button("4"));
-        container2.add(new Button("5"));
-        container2.add(new Button("6"));*/
-//        panel.add(container1);
         panel.add(instr);
         panel.add(targetColor);
         panel.add(container2);
         add(panel);
         setVisible(true);
+    }
+
+    private void addToRandomIntList(JButton[] jButtonArray, ArrayList<Integer> randIntList) {
+        for (int i = 0; i < jButtonArray.length; i++) {
+            int randInt = ThreadLocalRandom.current().nextInt(0, 25 + 1);
+            if (randIntList.size() < 1) {
+                randIntList.add(randInt);
+            } else {
+                for (int item : randIntList) {
+                    if (item != randInt) {
+                        randIntList.add(item);
+                    } else {
+                        int newInt = checkForDuplicate(item);
+                        randIntList.add(item);
+                        } while (item != randInt);
+                    }
+                }
+            }
+        }
+
+    private int checkForDuplicate(int item) {
+//        Boolean isUnique = false;
+        int randInt = ThreadLocalRandom.current().nextInt(0, 25 + 1);
+        if (item != randInt) {
+            // do nothing
+        } else {
+            checkForDuplicate(item);
+        }
+        return randInt;
+    }
+
+
+    private void addRandomInt(List<Integer> randIntList, JButton[] jButtonArray) {
+        int newInt = ThreadLocalRandom.current().nextInt(0, jButtonArray.length + 1);
+        if (randIntList.size() < 1) {
+            randIntList.add(newInt);
+        } else {
+//            checkForDuplicateRandomInt(newInt, (ArrayList<Integer>) randIntList, jButtonArray);
+        }
+    }
+
+    private void checkForDuplicateRandomInt(int newInt, ArrayList<Integer> randIntList, JButton[] jButtonArray) {
+        List<Integer> duplicateList = new ArrayList<Integer>();
+        /*for (int index : randIntList) {
+            duplicateList.add(randIntList.get(index));
+            System.out.println("Index " + index + " in duplicateList is: " + duplicateList.get(index));
+        }*/
+        for (int item : randIntList) {
+            if (item != newInt) {
+                duplicateList.add(newInt);
+            } else {
+                newInt = ThreadLocalRandom.current().nextInt(0, jButtonArray.length + 1);
+                checkForDuplicateRandomInt(newInt, randIntList, jButtonArray);
+            }
+        }
     }
 
     public void initGrid() {
@@ -132,7 +211,7 @@ public class MainGameFrame extends JFrame {
     }
 
     private String getTargetColor() {
-       colorIndex = ThreadLocalRandom.current().nextInt(0, 25 + 1);
+       colorIndex = ThreadLocalRandom.current().nextInt(0, 6);
        String colorStr = "";
         switch(colorIndex) {
             case 0:
